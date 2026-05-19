7532117003ab9f846539bbc6f446322015542ff2
Vyom Kulshrestha <hardikshobhit87@gmail.com>
2026-05-20 00:13:27 +0530

    style: fix indentation and type ignore syntax for tomllib fallback

--- daemon/pilot/config.py
+++ daemon/pilot/config.py
@@ -15,7 +15,7 @@ else:
     try:
         import tomllib
     except ModuleNotFoundError:
-            import tomli as tomllib # type : ignore{ no redef}
+        import tomli as tomllib  # type: ignore[no-redef]
 
 import tomli_w
 
